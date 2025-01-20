6c59556e38bc157f0bb0bd0d2ec9922c8e3ed382
ary1197 <24hs108@s.bgu.ac.jp>
2025-01-20 14:24:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,18 +9,18 @@
                 result = "少し大変";
             } else if (colour === "楽" && number === 2) {
                 result = "苦しい";
-            } else if (colour === "ピンク" && number === 0) {
+            } else if (colour === "苦しむ" && number === 0) {
                 result = "とても幸せ";
-            } else if (colour === "ピンク" && number === 1) {
-                result = "楽";
-            } else if (colour === "ピンク" && number === 2) {
+            } else if (colour === "苦しむ" && number === 1) {
+                result = "楽しい";
+            } else if (colour === "苦しむ" && number === 2) {
                 result = "すごく大変";
-            } else if (colour === "黒" && number === 0) {
-                result = "苦しい";
-            } else if (colour === "黒" && number === 1) {
+            } else if (colour === "平凡" && number === 0) {
+                result = "元気百倍";
+            } else if (colour === "平凡" && number === 1) {
                 result = "幸せな一日";
-            } else if (colour === "黒" && number === 2) {
-                result = "楽";
+            } else if (colour === "平凡" && number === 2) {
+                result = "無気力";
             }
 
             document.getElementById('result-output').innerText = "今日の一日は【" + result + "】です";
